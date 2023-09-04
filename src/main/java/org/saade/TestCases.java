78a998c3a8b1b685320f206f8edcecd733dd75c8
saadehm80 <107985594+saadehm80@users.noreply.github.com>
2023-09-05 01:15:59 +0200

    Update TestCases.java

--- src/main/java/org/saade/TestCases.java
+++ src/main/java/org/saade/TestCases.java
@@ -4,15 +4,15 @@ package org.saade;
 import org.testng.annotations.Test;
 
 public class TestCases {
-    @Test(priority = 2) //
+    @Test(priority = 1) //
     void setup (){
         System.out.println("I am inside setup");
     }
-    @Test(priority = 3)
+    @Test(priority = 2)
     void testSteps(){
         System.out.println("I am inside testSteps");
     }
-    @Test(priority = 1)
+    @Test(priority = 3)
     void tearDown(){
         System.out.println("I am inside tearDown");
     }
